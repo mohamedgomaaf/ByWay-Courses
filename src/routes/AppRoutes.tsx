703c06a28c44690dacb71e5476488c4c501005fb
Mohamed Gomaa <99934781+mohamedgomaaf@users.noreply.github.com>
2025-11-24 00:30:34 +0000

    fix: update import paths for course-related components to maintain consistency

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -68,12 +68,12 @@ import UserProfilePage from "@/pages/profile/UserProfilePage";
 import NotFound from "@/pages/NotFound/NotFound";
 import ReportsAnalytics from "@/components/AdminDashboard/Reports&Analytics/ReportsAnalytics";
 import ScrollToTop from "@/utils/ScrollToTop";
-import LearnerMyCourses from "@/pages/courses/MyCourses/LearnerMyCourses";
-import LearnerCourseDetails from "@/pages/courses/CourseDetails/LearnerCourseDetailes";
-import WatchVideo from "@/pages/courses/WatchVideo";
-import InstructorCourseDetails from "@/pages/courses/CourseDetails/InstructorCourseDetails";
-import MyCourses from "@/pages/courses/MyCourses/MyCourses";
-import CoursesPage from "@/pages/courses/CoursesPage";
+import LearnerMyCourses from "@/pages/Courses/MyCourses/LearnerMyCourses";
+import LearnerCourseDetails from "@/pages/Courses/CourseDetails/LearnerCourseDetailes";
+import WatchVideo from "@/pages/Courses/WatchVideo";
+import InstructorCourseDetails from "@/pages/Courses/CourseDetails/InstructorCourseDetails";
+import MyCourses from "@/pages/Courses/MyCourses/MyCourses";
+import CoursesPage from "@/pages/Courses/CoursesPage";
 
 
 export default function AppRoutes() {
